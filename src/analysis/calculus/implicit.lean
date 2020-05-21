53d76c841dfdba2384cff4011ebcad4b2280fb7f
Yury G. Kudryashov <urkud@urkud.name>
2020-05-21 03:51:42 -0400

    Do `of_proj` instead of `prod_`

--- src/analysis/calculus/implicit.lean
+++ src/analysis/calculus/implicit.lean
@@ -11,16 +11,15 @@ import analysis.normed_space.complemented
 
 We prove three versions of the implicit function theorem.
 
+* `implicit_function_of_proj` : TODO
+
 * `implicit_function_of_complemented`: implicit function defined by `f (g z y) = z`, where
   `f : E → F` is a function strictly differentiable at `a` such that its defivative `f'`
   is surjective and has a `complemented` kernel;
 
 * `implicit_function`: implicit function defined by `f (g z y) = z`, where `f : E → F` is a function
   with finitely dimensional codomain such that `f` is strictly differentiable at `a` and its
-  defivative `f'` is surjective;
-
-* `prod_implicit_function`: implicit function defined by `f (x, g(x, z)) = z`, where `f : E × F → G`
-  is a strictly differentiable function with invertible derivative `∂f/∂y`.
+  defivative `f'` is surjective.
 -/
 
 noncomputable theory
@@ -32,6 +31,87 @@ open continuous_linear_equiv (of_bijective)
 namespace has_strict_fderiv_at
 
 section generic
+
+variables {𝕜 : Type*} [nondiscrete_normed_field 𝕜]
+  {E : Type*} [normed_group E] [normed_space 𝕜 E] [complete_space E]
+  {F : Type*} [normed_group F] [normed_space 𝕜 F] [complete_space F]
+  {G : Type*} [normed_group G] [normed_space 𝕜 G] [complete_space G]
+  {f : E → F} {f' : E ≃L[𝕜] F × G} {a : E}
+
+lemma implicit_aux_deriv_of_proj
+  (hf : has_strict_fderiv_at f ((fst 𝕜 F G).comp f') a) :
+  has_strict_fderiv_at (λ x, (f x, (f' (x - a)).2)) (f' : E →L[𝕜] F × G) a :=
+begin
+  convert hf.prod ((continuous_linear_equiv.has_strict_fderiv_at f').comp a
+    ((has_strict_fderiv_at_id a).sub_const a)).snd,
+  ext x; simp
+end
+
+section defs
+
+variables (f f')
+
+def implicit_to_local_homeomorph_of_proj
+  (hf : has_strict_fderiv_at f ((fst 𝕜 F G).comp f') a) :
+  local_homeomorph E (F × G) :=
+(implicit_aux_deriv_of_proj hf).to_local_homeomorph _
+
+def implicit_function_of_proj
+  (hf : has_strict_fderiv_at f ((fst 𝕜 F G).comp f') a) :
+  F → G → E :=
+function.curry $ (hf.implicit_to_local_homeomorph_of_proj f f').symm
+
+end defs
+
+@[simp] lemma implicit_to_local_homeomorph_of_proj_apply
+  (hf : has_strict_fderiv_at f ((fst 𝕜 F G).comp f') a) (x : E) :
+  hf.implicit_to_local_homeomorph_of_proj f f' x = (f x, (f' (x - a)).2) :=
+rfl
+
+lemma mem_implicit_to_local_homeomorph_of_proj_source
+  (hf : has_strict_fderiv_at f ((fst 𝕜 F G).comp f') a) :
+  a ∈ (hf.implicit_to_local_homeomorph_of_proj f f').source :=
+mem_to_local_homeomorph_source _
+
+lemma mem_implicit_to_local_homeomorph_of_proj_target
+  (hf : has_strict_fderiv_at f ((fst 𝕜 F G).comp f') a) :
+  (f a, (0 : G)) ∈ (hf.implicit_to_local_homeomorph_of_proj f f').target :=
+by simpa using
+  ((hf.implicit_to_local_homeomorph_of_proj f f').map_source $
+    (hf.mem_implicit_to_local_homeomorph_of_proj_source))
+
+/-- `implicit_function_of_complemented` sends `(z, y)` to a point in `f ⁻¹' z`. -/
+lemma map_implicit_function_of_proj_eq
+  (hf : has_strict_fderiv_at f ((fst 𝕜 F G).comp f') a) :
+  ∀ᶠ (p : F × G) in 𝓝 (f a, 0),
+    f (hf.implicit_function_of_proj f f' p.1 p.2) = p.1 :=
+((hf.implicit_to_local_homeomorph_of_proj f f').eventually_right_inverse $
+  hf.mem_implicit_to_local_homeomorph_of_proj_target).mono $ λ ⟨z, y⟩ h,
+    congr_arg prod.fst h
+
+/-- Any point in some neighborhood of `a` can be represented as `implicit_function`
+of some point. -/
+lemma eq_implicit_function_of_proj
+  (hf : has_strict_fderiv_at f ((fst 𝕜 F G).comp f') a) :
+  ∀ᶠ x in 𝓝 a, hf.implicit_function_of_proj f f' (f x)
+    (hf.implicit_to_local_homeomorph_of_proj f f' x).snd = x :=
+hf.implicit_aux_deriv_of_proj.eventually_left_inverse
+
+lemma to_implicit_function_of_proj (hf : has_strict_fderiv_at f ((fst 𝕜 F G).comp f') a)
+  (f'inv : G →L[𝕜] E) (hf'inv : ∀ z : G, f' (f'inv z) = (0, z)) :
+  has_strict_fderiv_at (hf.implicit_function_of_proj f f' (f a)) f'inv 0 :=
+begin
+  have := hf.implicit_aux_deriv_of_proj.to_local_inverse,
+  simp only [sub_self, continuous_linear_equiv.map_zero, prod.snd_zero] at this,
+  convert this.comp 0
+    ((has_strict_fderiv_at_const (f a) 0).prod $ has_strict_fderiv_at_id 0),
+  ext x,
+  simp [continuous_linear_equiv.eq_symm_apply, hf'inv]
+end
+
+end generic
+
+section complemented
 
 variables {𝕜 : Type*} [nondiscrete_normed_field 𝕜]
   {E : Type*} [normed_group E] [normed_space 𝕜 E] [complete_space E]
@@ -128,7 +208,7 @@ begin
   simp [continuous_linear_equiv.eq_symm_apply, classical.some_spec hker]
 end
 
-end generic
+end complemented
 
 section finite_dimensional
 
@@ -194,89 +274,5 @@ by apply to_implicit_function_of_complemented
 
 end finite_dimensional
 
-/-!
-### Implicit function theorem for `f : E × F → G`
-
-Now we prove the implicit function theorem for a function `f : E × F → G` that has a derivative
-`f' : E × F →L[𝕜] G` in the strict sense and the derivative `∂f/∂y : F →L[𝕜] G` is invertible.
--/
-
-section product
-
-variables {𝕜 : Type*} [nondiscrete_normed_field 𝕜]
-  {E : Type*} [normed_group E] [normed_space 𝕜 E]
-  {F : Type*} [normed_group F] [normed_space 𝕜 F]
-  {G : Type*} [normed_group G] [normed_space 𝕜 G]
-
-variables [cs : complete_space (E × F)] {f : E × F → G} (f' : E × F →L[𝕜] G) (f'inv : G →L[𝕜] F)
-  {p : E × F} (hf : has_strict_fderiv_at f f' p)
-  (hf'l : ∀ y : F, f'inv (f' (0, y)) = y) (hf'r : ∀ z : G, f' (0, f'inv z) = z)
-
-/-- Formula for the derivative of an implicit function. -/
-def prod_implicit_function_fderiv : (E × G) →L[𝕜] F :=
-(f'inv.comp $ continuous_linear_map.snd 𝕜 E G -
-      f'.comp ((continuous_linear_map.id 𝕜 E).prod_map 0))
-
-variables {f' f'inv}
-
-@[simp] lemma prod_implicit_function_fderiv_apply (x) :
-  prod_implicit_function_fderiv f' f'inv x = f'inv (x.2 - f' (x.1, 0)) := rfl
-
-include hf'r
-
-lemma prod_implicit_fderiv_right_inverse (x : E) (z : G) :
-  f' (x, f'inv z) = f' (x, 0) + z :=
-by { conv_rhs { rw [← hf'r z] }, simp [← f'.map_add] }
-
-include hf'l
-
-variables (f' f'inv)
-
-/-- Derivative of an auxiliary function used in the proof of the implicit function theorem. -/
-def prod_implicit_function_aux_fderiv : (E × F) ≃L[𝕜] (E × G) :=
-continuous_linear_equiv.equiv_of_inverse
-  ((continuous_linear_map.fst 𝕜 E F).prod f')
-  ((continuous_linear_map.fst 𝕜 E G).prod $ prod_implicit_function_fderiv f' f'inv)
-  (λ ⟨x, y⟩, by simp [← continuous_linear_map.map_sub, hf'l])
-  (λ ⟨x, z⟩, by simp [-continuous_linear_map.map_sub, prod_implicit_fderiv_right_inverse hf'r])
-
-variables {f' f'inv}
-
-include hf
-
-lemma prod_implicit_function_aux_deriv :
-  has_strict_fderiv_at (λ x : E × F, (x.1, f x))
-    (prod_implicit_function_aux_fderiv f' f'inv hf'l hf'r : (E × F) →L[𝕜] E × G) p :=
-has_strict_fderiv_at_fst.prod hf
-
-include cs
-variable (f)
-
-/-- Implicit function `g` defined by an equation `f (x, g(x, z)) = z`. -/
-def prod_implicit_function (x : E × G) : F :=
-((hf.prod_implicit_function_aux_deriv hf'l hf'r).local_inverse _ _ _ x).2
-
-lemma prod_implicit_function_def (x : E × G) :
-  hf.prod_implicit_function f hf'l hf'r x =
-    ((hf.prod_implicit_function_aux_deriv hf'l hf'r).local_inverse _ _ _ x).2 :=
-rfl
-
-lemma to_prod_implicit_function :
-  has_strict_fderiv_at (hf.prod_implicit_function f hf'l hf'r)
-    (prod_implicit_function_fderiv f' f'inv) (p.1, f p) :=
-((hf.prod_implicit_function_aux_deriv hf'l hf'r).to_local_inverse).snd
-
-lemma eventually_apply_fst_prod_implicit_function_eq :
-  ∀ᶠ (x : E × G) in 𝓝 (p.1, f p), f (x.1, hf.prod_implicit_function f hf'l hf'r x) = x.2 :=
-(hf.prod_implicit_function_aux_deriv hf'l hf'r).eventually_right_inverse.mono $
-  λ x hx, by { convert congr_arg prod.snd hx, convert prod.mk.eta,
-    exact (congr_arg prod.fst hx).symm }
-
-lemma eventually_prod_implicit_function_eq :
-  ∀ᶠ (x : E × F) in 𝓝 p, hf.prod_implicit_function f hf'l hf'r (x.1, f x) = x.2 :=
-(hf.prod_implicit_function_aux_deriv hf'l hf'r).eventually_left_inverse.mono $
-  λ x hx, congr_arg prod.snd hx
-
-end product
 
 end has_strict_fderiv_at
